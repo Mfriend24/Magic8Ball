94572e65e2c03fdc8c3c2a9af87f552aee626781
Matthew Friend <lindafriend@Matthews-MacBook-Pro-2.local>
2014-08-09 10:23:03 -0500

    first commit

---
+++ app/magic_8ball_view_controller.rb
@@ -0,0 +1,13 @@
+class Magic8BallViewController < UIViewController
+  def loadView
+    self.view = UIImageView.alloc.init 
+  end
+  
+  def viewDidLoad
+    view.image = UIImage.imageNamed('background.png')
+     
+     @label = UILabel.alloc.initWithFrame([[10, 60], [300, 80]] )
+    view.addSubview(@label) 
+ 
+ end
+end 
